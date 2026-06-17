1981f9501f72fcd807f22894caa0543bfe3e1c4a
chkarthik <chellurikarthik2025@gmail.com>
2026-06-17 10:40:52 +0530

    nineth commit

---
+++ product all intergers exclude itself.py
@@ -0,0 +1,18 @@
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        product, zero_cnt = 1, 0
+        for num in nums:
+            if num != 0:
+                product *= num
+            else:
+                zero_cnt += 1
+        
+        if zero_cnt > 1:
+            return [0]*len(nums)
+        
+        if zero_cnt != 0:
+            return [0 if num != 0 else int(product) for num in nums] 
+        else:
+            return [int(product/num) for num in nums]
