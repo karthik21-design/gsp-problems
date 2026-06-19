bc473767f753cf15d5040173e0082a317b4ea15f
chkarthik <chellurikarthik2025@gmail.com>
2026-06-19 12:18:07 +0530

    eleventh commit

---
+++ rotate numbers by k.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        for _ in range(k):
+            last = nums.pop()
+            nums.insert(0, last)
